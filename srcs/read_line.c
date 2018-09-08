6e792ea9802f14c93a34c6b33efb97f83c3f76e0
Olena BONDARENKO <olbondar@student.unit.ua>
2018-09-08 19:10:24 +0300

    parser

--- srcs/read_line.c
+++ srcs/read_line.c
@@ -11,14 +11,17 @@ void read_line(char *line, int y, int **map, t_env *env)
 		printf("%d\n", 6);
 		map_error();
 	}
-	puts(line);
 	str_splitted = ft_strsplit(line, ' ');
+/******************************************************************/
+// puts(str_splitted[x]);
+// write(1, &str_splitted[x][0], 1);
+// write(1, "\n", 1);
+// printf("ft_atoi(str_splitted[x]): %d\n", ft_atoi(str_splitted[x]));
+/******************************************************************/
 	map[y] = (int *)malloc(sizeof(int *) * env->width);
 	while (str_splitted[x] != '\0')
 	{
-		printf("str_splitted[x][0]: %d", str_splitted[x][0]);
-		// printf("map_width: %d", env->map_width);
-		if (!(str_splitted[x][0] >= '0' && str_splitted[x][0] <= '9' && ft_atoi(str_splitted[x]) >= '0' && x < env->map_width))
+		if (!(str_splitted[x][0] >= '0' && str_splitted[x][0] <= '9' && ft_atoi(str_splitted[x]) >= 0 /*&& x < env->map_width*/))
 		{
 			printf("\nmap_error: %d\n", 7);
 			map_error();
@@ -27,6 +30,10 @@ void read_line(char *line, int y, int **map, t_env *env)
 	}
 	if (x != env->map_width)
 	{
+printf("x: %d\n", x);
+printf("env->map_width: %d\n", env->map_width);
+
+
 		printf("%d\n", 8);
 		map_error();
 	}
