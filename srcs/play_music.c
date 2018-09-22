f6cb5698e21115df664a757cc9cc832c69126fb1
Elena Bondareko <bondarenkoelenaaa@gmail.com>
2018-09-22 18:03:09 +0300

    switch music

--- srcs/play_music.c
+++ srcs/play_music.c
@@ -1,5 +1,7 @@
 /*
 afplay is a Mac command which plays audio file, where the last character ''&'' indicates afplay to run in the background as a job.
+system("afplay ../music/music1.mp3 &");
+---------------------------------------
 mpg123 is analogue for Linux
 */
 
@@ -12,23 +14,11 @@ void	play_music()
 	srand(time(NULL));
 	random = rand() % 4;
 	if (random == 1)
-	{
-		// system("afplay ../music/music1.mp3 &");
 		system("mpg123  music/music1.mp3 &");
-	}
 	if (random == 2)
-	{
-		// system("afplay ../music/music2.mp3 &");
 		system("mpg123  music/music2.mp3 &");
-	}
 	if (random == 3)
-	{
-		// system("afplay ../music/music3.mp3 &");
 		system("mpg123  music/music3.mp3 &");
-	}
 	if (random == 4)
-	{
-		// system("afplay ../music/music4.mp3 &");
 		system("mpg123  music/music4.mp3 &");
-	}
 }
