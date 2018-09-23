ca04675da591f7306ca298cc50fe37d43bc69218
Elena Bondareko <bondarenkoelenaaa@gmail.com>
2018-09-23 17:21:02 +0300

    textures

--- srcs/get_color.c
+++ srcs/get_color.c
@@ -4,15 +4,12 @@ unsigned int	get_color(t_env *env)
 {
 	if (env->ray.hit_side == 1)
 	{
-		if ((env->ray.step.x == -1 && env->ray.step.y == -1) ||
-			(env->ray.step.x == 1 && env->ray.step.y == -1))
+		if ((env->ray.step.x == -1 && env->ray.step.y == -1) ||	(env->ray.step.x == 1 && env->ray.step.y == -1))
 			return (env->color_1);
-		if ((env->ray.step.x == -1 && env->ray.step.y == 1) ||
-			(env->ray.step.x == 1 && env->ray.step.y == 1))
+		if ((env->ray.step.x == -1 && env->ray.step.y == 1) ||	(env->ray.step.x == 1 && env->ray.step.y == 1))
 			return (env->color_2);
 	}
-	if ((env->ray.step.x == -1 && env->ray.step.y == -1) ||
-		(env->ray.step.x == -1 && env->ray.step.y == 1))
+	if ((env->ray.step.x == -1 && env->ray.step.y == -1) ||	(env->ray.step.x == -1 && env->ray.step.y == 1))
 		return (env->color_3);
 	return (env->color_4);
 }
