a78e46dc83f42b7465d3c44e092d487a6adca8d8
Elena Bondareko <bondarenkoelenaaa@gmail.com>
2018-09-27 11:18:09 +0300

    textured

--- srcs/draw_line.c
+++ srcs/draw_line.c
@@ -1,3 +1,16 @@
+/*
+bricks									---> put_pixel(env, x, i, c * (x % 16 && i % 16));
+abstract weave blue						---> put_pixel(env, x, i, 65536 *  i * x * 256 / ((end + env->player.z) * env->height));
+abstract weave red						---> put_pixel(env, x, i, 65536 *  i * x * 4 / (env->height / end));
+abstract strips 						---> put_pixel(env, x, i, 65536 *  i * x * 4 / ((end + env->player.z) * env->width));
+abstract weave + abstract strips red	---> put_pixel(env, x, i, 65536 *  i * x * 4 / ((end) - env->width));
+abstract weave + abstract strips green	---> put_pixel(env, x, i, 65536 *  i * x * 4 / (env->width));
+
+gradient 								---> put_pixel(env, x, i, c >> 5);
+black 0x000000
+white 0xffffff
+*/
+
 #include "../includes/wolf3d.h"
 
 void			draw_line(t_env *env, int x, int start, int end)
@@ -9,39 +22,24 @@ void			draw_line(t_env *env, int x, int start, int end)
 	c = add_smog(c, env->ray.distance);
 	i = -1;
 
-	// sky //
 	while (++i < start + env->player.z)
-	{
-		if (env->switch_textures == 0)
-			put_pixel(env, x, i, env->color_sky);
-	}
+		put_pixel(env, x, i, env->color_sky);
 	i--;
-	// ground //
-	while (++i <= end + env->player.z && i < env->height)
-	{
-		if (env->switch_textures == 0)
-			put_pixel(env, x, i, c);
-		if (env->switch_textures == 1)
-		{
-			put_pixel(env, x, i, 65536 *  i * x * 4 / (env->height / end));
-		}
-	}
+	while (++i <= end + env->player.z && i < env->height && env->switch_textures == 0)
+		put_pixel(env, x, i, c);
+	while (++i <= end + env->player.z && i < env->height && env->switch_textures == 1)
+		put_pixel(env, x, i, c * (x % 16 && i % 16));
+	while (++i <= end + env->player.z && i < env->height && env->switch_textures == 2)
+		put_pixel(env, x, i, 65536 *  i * x * 256 / ((end + env->player.z) * env->height));
+	while (++i <= end + env->player.z && i < env->height && env->switch_textures == 3)
+		put_pixel(env, x, i, 65536 *  i * x * 4 / (env->height / end));
+	while (++i <= end + env->player.z && i < env->height && env->switch_textures == 4)
+		put_pixel(env, x, i, 65536 *  i * x * 4 / ((end + env->player.z) * env->width));
+	while (++i <= end + env->player.z && i < env->height && env->switch_textures == 5)
+		put_pixel(env, x, i, 65536 *  i * x * 4 / ((end) - env->width));
+	while (++i <= end + env->player.z && i < env->height && env->switch_textures == 6)
+		put_pixel(env, x, i, 65536 *  i * x * 4 / (env->width));
 	i--;
-	// walls //
 	while (++i < env->height)
 		put_pixel(env, x, i, add_smog(env->color_ground, abs(i - env->height) * 0.040));
 }
-
-
-/*
-gradient 								---> put_pixel(env, x, i, c >> 5);
-bricks									---> put_pixel(env, x, i, c * (x % 16 && i % 16));
-abstract weave blue						---> put_pixel(env, x, i, 65536 *  i * x * 256 / ((end + env->player.z) * env->height));
-abstract weave red						---> put_pixel(env, x, i, 65536 *  i * x * 4 / (env->height / end));
-abstract strips 						---> put_pixel(env, x, i, 65536 *  i * x * 4 / ((end + env->player.z) * env->width));
-abstract weave + abstract strips red	---> put_pixel(env, x, i, 65536 *  i * x * 4 / ((end) - env->width));
-abstract weave + abstract strips green	---> put_pixel(env, x, i, 65536 *  i * x * 4 / (env->width));
-
-black 0x000000
-white 0xffffff
-*/
--- srcs/get_color.c
+++ srcs/get_color.c
@@ -5,21 +5,11 @@ unsigned int	get_color(t_env *env)
 	if (env->ray.hit_side == 1)
 	{
 		if ((env->ray.step.x == -1 && env->ray.step.y == -1) ||	(env->ray.step.x == 1 && env->ray.step.y == -1))
-		{
-			// env->switch_textures = 1;
 			return (env->color_1);
-		}
 		if ((env->ray.step.x == -1 && env->ray.step.y == 1) ||	(env->ray.step.x == 1 && env->ray.step.y == 1))
-		{
-			// env->switch_textures = 2;
 			return (env->color_2);
-		}
 	}
 	if ((env->ray.step.x == -1 && env->ray.step.y == -1) ||	(env->ray.step.x == -1 && env->ray.step.y == 1))
-	{
-		// env->switch_textures = 3;
 		return (env->color_3);
-	}
-	// env->switch_textures = 4;
 	return (env->color_4);
 }
