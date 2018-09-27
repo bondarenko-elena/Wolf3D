3bc1f83cb79ad6caff2cfe85a8abbce1aa1c3b41
Elena Bondareko <bondarenkoelenaaa@gmail.com>
2018-09-27 14:00:17 +0300

    draw_line.c is fixed

--- srcs/draw_line.c
+++ srcs/draw_line.c
@@ -27,16 +27,22 @@ void			draw_line(t_env *env, int x, int start, int end)
 	i--;
 	while (++i <= end + env->player.z && i < env->height && env->switch_textures == 0)
 		put_pixel(env, x, i, c);
+	i--;
 	while (++i <= end + env->player.z && i < env->height && env->switch_textures == 1)
 		put_pixel(env, x, i, c * (x % 16 && i % 16));
+	i--;
 	while (++i <= end + env->player.z && i < env->height && env->switch_textures == 2)
 		put_pixel(env, x, i, 65536 *  i * x * 256 / ((end + env->player.z) * env->height));
+	i--;
 	while (++i <= end + env->player.z && i < env->height && env->switch_textures == 3)
 		put_pixel(env, x, i, 65536 *  i * x * 4 / (env->height / end));
+	i--;
 	while (++i <= end + env->player.z && i < env->height && env->switch_textures == 4)
 		put_pixel(env, x, i, 65536 *  i * x * 4 / ((end + env->player.z) * env->width));
+	i--;
 	while (++i <= end + env->player.z && i < env->height && env->switch_textures == 5)
 		put_pixel(env, x, i, 65536 *  i * x * 4 / ((end) - env->width));
+	i--;
 	while (++i <= end + env->player.z && i < env->height && env->switch_textures == 6)
 		put_pixel(env, x, i, 65536 *  i * x * 4 / (env->width));
 	i--;
